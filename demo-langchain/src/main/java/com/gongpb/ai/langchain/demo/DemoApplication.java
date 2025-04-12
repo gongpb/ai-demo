e37fdd79dbd08da6c95c499e9552b307d2ba7921
gongpibao <gongpibao@jd.com>
2025-04-12 21:52:28 +0800

    demo code

--- demo-langchain/src/main/java/com/gongpb/ai/langchain/demo/DemoApplication.java
+++ demo-langchain/src/main/java/com/gongpb/ai/langchain/demo/DemoApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 // 告诉框架这是SpringBoot应用
 @SpringBootApplication
